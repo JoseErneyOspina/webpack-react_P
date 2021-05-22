cc2a100c41b6f272d26df4be0a6866374c233fc2
Jose Erney Ospina <JoseErneyOsp@gmail.com>
2021-05-22 18:54:43 -0500

    [Ready] Config for deploy

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import './styles/global.scss';
 
-ReactDom.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
